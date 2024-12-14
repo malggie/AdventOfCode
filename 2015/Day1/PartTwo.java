eb64e27f2f8738238826503b133a2abaac064b6a
malggie <131193254+malggie@users.noreply.github.com>
2024-12-14 09:55:26 +0100

    Update PartTwo.java

--- 2015/Day1/PartTwo.java
+++ 2015/Day1/PartTwo.java
@@ -1,8 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
 
     public static void main(String[] args) {
