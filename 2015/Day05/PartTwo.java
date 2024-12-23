8239e903a03bad6695ee54e337ba9a53b0feb6d1
malggie <131193254+malggie@users.noreply.github.com>
2024-12-23 14:39:34 +0100

    Add files via upload

---
+++ 2015/Day05/PartTwo.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String filePath = "C:/Code/text.txt";
+        ArrayList<String> expressions = getInput(filePath);
+        System.out.println(countNiceString(expressions));
+    }
+
+    private static ArrayList<String> getInput(String filePath) {
+        ArrayList<String> array = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(filePath))){
+            while (scanner.hasNextLine()) {
+                array.add(scanner.nextLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return array;
+    }
+
+    private static int countNiceString(ArrayList<String> array) {
+        int counter = 0;
+        for (String word : array) {
+            if (word.matches(".*([a-z]{2}).*?\\1.*") && word.matches(".*([a-z]).\\1.*")) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
